e1a2895547127e021e8a854c39722db0c7e6d9ca
Ryan Drake <ryan@ryandrake.com>
2016-03-07 13:06:18 +1000

    Update insertImage function with correct reference to ContentTools image

--- build/content-tools.js
+++ build/content-tools.js
@@ -6326,10 +6326,13 @@
 
         ImageDialog.prototype.insertImage = function(imageURL) {
             this._imageURL = imageURL;
-            var div = document.createElement('p');
-            div.className = 'class="ce-element ce-element--type-text ce-element--focused';
-            div.innerHTML = '<img src="' + this._imageURL + '" />';
-            document.getElementById('content-tools-editor').appendChild(div);
+            // var div = document.createElement('p');
+            // div.className = 'class="ce-element ce-element--type-text ce-element--focused';
+            // div.innerHTML = '<img src="' + this._imageURL + '" />';
+            var image = new ContentEdit.Image(
+              {src: this._imageURL, alt=''}              
+            );
+            document.getElementById('content-tools-editor').appendChild(image);
         };
 
         ImageDialog.prototype.state = function(state) {
